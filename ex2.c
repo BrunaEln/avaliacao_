31883d875f98ce1a9dbc0b5817610e1b54da5b7b
brunaelen <brunaelen365@gmail.com>
2023-05-13 21:48:38 -0300

    att codigo

--- ex2.c
+++ ex2.c
@@ -7,19 +7,22 @@ int main () {
     int resposta, pecas, quantidade;
     float mostraFinal;
 
-    do {
+    while (resposta != 2) {
     printf ("Digite o numero de pecas fabricadas por voce esse mes: ");
-    scanf ("%f", &pecas);
-    if (pecas >= 50) {
+    scanf ("%d", &pecas);
+
+    if (pecas > 50) {
     quantidade = validaQuantidade (pecas);
     system ("cls");
     printf ("Numero de pecas validas para o calculo de acrescimo ---> %d pcs", quantidade);
     mostraFinal = calculaSalario (pecas);
     printf ("\nSalario atualizado %.2f\n", mostraFinal);
+    } else {
+        printf ("Numero de pecas insuficientes para a validacao");
+    }
     printf ("\nDeseja calcular o salario de outro funcionario? [1] para sim | [2] para nao\n");
     scanf ("%d", &resposta);
     }
-    } while (resposta = 2);
 }
 
 int validaQuantidade (int quantidade) {
@@ -31,10 +34,7 @@ int validaQuantidade (int quantidade) {
 float calculaSalario (float salario) {
     float resultado, resultado1, resto, resultado2, resultadoprov, resultadofinal;
     int c = 1, i = 1;
-    if (salario <= 50) {
-        resultado = 600.00;
-        return resultado; 
-    } else if (salario > 50 && salario <= 80) {
+    if (salario > 50 && salario <= 80) {
         resultadoprov = salario - 50; 
         resultado = 600 + (resultadoprov * 0.5);
         return resultado;
--- ex3.c
+++ ex3.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 
 main () {
-int jogadores [6];
-int totidades = 0;
-float mediaI, mediaP, porcentagem;
-int menor = 0, mais = 0, totpesos = 0;
 
+int jogadores [6];
+float pesos [6];
+float totidades = 0; //armazena a totalidade das idades dos jogadores.
+float mediaI, mediaP, porcentagem, oitenta = 0; 
+int menor = 0, mais = 0, totpesos = 0, numJogadores = 0;
 
 for (int i = 0; i < 6; i++) {
     printf ("Digite a idade do jogador: ");
     scanf ("%d", &jogadores[i]);
+    printf ("Digite o peso do jogador: ");
+    scanf ("%f", &pesos[i]);       
     totidades = totidades + jogadores [i];
-        for (int c = 0; c < 1; c++) {
-            printf ("Digite o peso do jogador: ");
-            scanf ("%f", &jogadores[c]);
-            if (jogadores [i] > 25) {
-                totpesos = totpesos + jogadores[c];
-            }
-            if (jogadores [c] > 80) {
-                mais++;
-            }
+
+        if (jogadores [i] > 25) {
+            totpesos = totpesos + pesos[i];
+            numJogadores++; //totaliza o número de jogadores com mais de 25 anos.
         }
-            if (jogadores [i] < 18) {
-                menor++;
+        if (pesos[i] > 80) {
+            oitenta = oitenta + pesos[i];
+            mais++; //totaliza os jogadores com o peso > que oitenta quilos.
         }
-porcentagem = mais/100;
-mediaI = totidades/6;
-mediaP = totpesos/6;
+        if (jogadores [i] < 18) { 
+            menor++; //totaliza jogadores menores de idade.
+        }
+
+porcentagem = (100 * mais)/6; //calculo da porcentagem.
+mediaI = totidades/6;  //media das idades.
+mediaP = totpesos/numJogadores; //calculo da média dos pesos.
 }
-    printf ("Media das idades = %.2f\nMedia peso: %.2f\nMenores de idade: %d\nPorcentagem: %.2f", mediaI, mediaP, menor, porcentagem);
+    printf ("Media das idades = %.2f\nMedia de pesos: %.2f\nMenores de idade: %d\nQuantidade de jogadores acima de 80 kg: %d\nPorcentagem: %.2f%%\n", mediaI, mediaP, menor, mais, porcentagem);
+}
